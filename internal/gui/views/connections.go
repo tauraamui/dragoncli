4038454d810beb9dc756dfd7111d98af0c50d44b
tauraamui <adamplewis@pm.me>
2021-02-26 12:58:37 +0000

    Correct render cache logic

--- internal/gui/views/connections.go
+++ internal/gui/views/connections.go
@@ -22,47 +22,50 @@ func (c Connections) Name() string {
 }
 
 func (c *Connections) Render() tview.Primitive {
+	if c.rendered != nil {
+		return c.rendered
+	}
+
 	connections, err := c.fetchConnections()
 	if err != nil {
 		logging.Error("Unable to fetch active connections: %v", err)
 	}
-	if c.rendered == nil {
-		rows := len(connections)
-		table := tview.NewTable()
+	rows := len(connections)
+	table := tview.NewTable()
 
-		table.SetCell(0, 0,
-			tview.NewTableCell("UUID").
+	table.SetCell(0, 0,
+		tview.NewTableCell("UUID").
+			SetAlign(tview.AlignCenter),
+	)
+
+	table.SetCell(0, 1,
+		tview.NewTableCell("TITLE").
+			SetAlign(tview.AlignCenter),
+	)
+
+	table.SetCell(0, 2,
+		tview.NewTableCell("DISK SIZE").
+			SetAlign(tview.AlignCenter),
+	)
+
+	for r := 0; r < rows; r++ {
+		conn := connections[r]
+		table.SetCell(r+1, 0,
+			tview.NewTableCell(conn.UUID).
 				SetAlign(tview.AlignCenter),
 		)
 
-		table.SetCell(0, 1,
-			tview.NewTableCell("TITLE").
+		table.SetCell(r+1, 1,
+			tview.NewTableCell(conn.Title).
 				SetAlign(tview.AlignCenter),
 		)
 
-		table.SetCell(0, 2,
-			tview.NewTableCell("DISK SIZE").
+		table.SetCell(r+1, 2,
+			tview.NewTableCell(conn.Size).
 				SetAlign(tview.AlignCenter),
 		)
-
-		for r := 0; r < rows; r++ {
-			conn := connections[r]
-			table.SetCell(r+1, 0,
-				tview.NewTableCell(conn.UUID).
-					SetAlign(tview.AlignCenter),
-			)
-
-			table.SetCell(r+1, 1,
-				tview.NewTableCell(conn.Title).
-					SetAlign(tview.AlignCenter),
-			)
-
-			table.SetCell(r+1, 2,
-				tview.NewTableCell(conn.Size).
-					SetAlign(tview.AlignCenter),
-			)
-		}
-		c.rendered = table
 	}
+	c.rendered = table
+
 	return c.rendered
 }
