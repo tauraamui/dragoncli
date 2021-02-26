c5db3e2ffa371cf449ead39e8aa61f918508a727
tauraamui <adamplewis@pm.me>
2021-02-26 16:47:27 +0000

    Improve connection list styling

--- internal/gui/views/connections.go
+++ internal/gui/views/connections.go
@@ -31,38 +31,39 @@ func (c *Connections) Render() tview.Primitive {
 		logging.Error("Unable to fetch active connections: %v", err)
 	}
 	rows := len(connections)
-	table := tview.NewTable()
+	table := tview.NewTable().
+		SetBorders(true)
 
 	table.SetCell(0, 0,
 		tview.NewTableCell("UUID").
-			SetAlign(tview.AlignCenter),
+			SetAlign(tview.AlignLeft),
 	)
 
 	table.SetCell(0, 1,
 		tview.NewTableCell("TITLE").
-			SetAlign(tview.AlignCenter),
+			SetAlign(tview.AlignLeft),
 	)
 
 	table.SetCell(0, 2,
-		tview.NewTableCell("DISK SIZE").
-			SetAlign(tview.AlignCenter),
+		tview.NewTableCell("SIZE ON DISK").
+			SetAlign(tview.AlignLeft),
 	)
 
 	for r := 0; r < rows; r++ {
 		conn := connections[r]
 		table.SetCell(r+1, 0,
-			tview.NewTableCell(conn.UUID).
-				SetAlign(tview.AlignCenter),
+			tview.NewTableCell(conn.UUID),
+			// SetAlign(tview.AlignRight),
 		)
 
 		table.SetCell(r+1, 1,
-			tview.NewTableCell(conn.Title).
-				SetAlign(tview.AlignCenter),
+			tview.NewTableCell(conn.Title),
+			// SetAlign(tview.AlignRight),
 		)
 
 		table.SetCell(r+1, 2,
-			tview.NewTableCell(conn.Size).
-				SetAlign(tview.AlignCenter),
+			tview.NewTableCell(conn.Size),
+			// SetAlign(tview.AlignRight),
 		)
 	}
 	c.rendered = table
