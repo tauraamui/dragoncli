e67f4bf325502fea2c684b8f703169be2d10223b
tauraamui <adamplewis@pm.me>
2021-02-26 11:11:11 +0000

    Include connection current persist size on disk to info table

--- internal/gui/views/connections.go
+++ internal/gui/views/connections.go
@@ -27,11 +27,6 @@ func (c *Connections) Render() tview.Primitive {
 		logging.Error("Unable to fetch active connections: %v", err)
 	}
 	if c.rendered == nil {
-		// list := tview.NewList()
-		// for _, conn := range connections {
-		// 	list.AddItem(conn.UUID, conn.Title, 'a', nil)
-		// }
-		// c.rendered = list
 		rows := len(connections)
 		table := tview.NewTable()
 
@@ -45,6 +40,11 @@ func (c *Connections) Render() tview.Primitive {
 				SetAlign(tview.AlignCenter),
 		)
 
+		table.SetCell(0, 1,
+			tview.NewTableCell("SIZE").
+				SetAlign(tview.AlignCenter),
+		)
+
 		for r := 0; r < rows; r++ {
 			conn := connections[r]
 			table.SetCell(r+1, 0,
@@ -54,6 +54,11 @@ func (c *Connections) Render() tview.Primitive {
 
 			table.SetCell(r+1, 1,
 				tview.NewTableCell(conn.Title).
+					SetAlign(tview.AlignCenter),
+			)
+
+			table.SetCell(r+1, 2,
+				tview.NewTableCell(conn.Size).
 					SetAlign(tview.AlignCenter),
 			)
 		}
